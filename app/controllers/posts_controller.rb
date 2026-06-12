fb3368dc08d00068dd3e3d953478bb01e869dfaa
takuya1 <oktkgm1997@icloud.com>
2026-06-13 00:41:01 +0900

    Lint修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = Post.includes(training: [:target, :ai_feedback])
+    @posts = Post.includes(training: [ :target, :ai_feedback ])
                  .order(created_at: :desc)
   end
 
