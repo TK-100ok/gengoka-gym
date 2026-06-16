421d7ef003232232fac0aa9a25595417304d688b
takuya1 <oktkgm1997@icloud.com>
2026-06-16 23:34:46 +0900

    feat: お気に入り登録と解除処理を実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,28 +5,13 @@ class FavoritesController < ApplicationController
   end
 
   def create
-    training = Training.find(params[:training_id])
-
-    current_user.favorites.create!(
-      training: training
-    )
-
-    redirect_back(
-      fallback_location: trainings_path,
-      notice: "お気に入りに登録しました"
-    )
+    @training = Training.find(params[:training_id])
+    current_user.favorites.create!(training: @training)
   end
 
   def destroy
-    favorite = current_user.favorites.find_by!(
-      training_id: params[:training_id]
-    )
-
+    favorite = current_user.favorites.find(params[:id])
+    @training = favorite.training
     favorite.destroy
-
-    redirect_back(
-      fallback_location: trainings_path,
-      notice: "お気に入りを解除しました"
-    )
   end
 end
