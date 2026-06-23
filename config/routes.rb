0634126f7a3b4b562a0b4ed145cdf54bd0e92fd9
takuya1 <oktkgm1997@icloud.com>
2026-06-23 18:35:21 +0900

    feat: アカウント設定画面をカスタマイズ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get "settings/index"
-  devise_for :users
+  devise_for :users,
+    controllers: {
+      registrations: "users/registrations"
+    }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "top#index"
 
