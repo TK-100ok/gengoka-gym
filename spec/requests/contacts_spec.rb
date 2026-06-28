42951f56c8b36cc1aff2035f1c18127e8b1a05a3
takuya1 <oktkgm1997@icloud.com>
2026-06-28 19:50:02 +0900

    test: お問い合わせ画面のリクエストスペックを修正

--- spec/requests/contacts_spec.rb
+++ spec/requests/contacts_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Contacts", type: :request do
-  describe "GET /show" do
-    it "returns http success" do
-      get "/contacts/show"
-      expect(response).to have_http_status(:success)
-    end
+  let(:user) { create(:user) }
+
+  before do
+    sign_in user
   end
 
+  it "returns http success" do
+    get contact_path
+    expect(response).to have_http_status(:success)
+  end
 end
