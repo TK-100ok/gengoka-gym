3badff8099cd180bbe5fb2eb609425ad5e49630e
takuya1 <oktkgm1997@icloud.com>
2026-07-07 23:45:45 +0900

    fix: ThinkingModesのリクエストスペックを認証対応

--- app/controllers/thinking_modes_controller.rb
+++ app/controllers/thinking_modes_controller.rb
@@ -1,4 +1,5 @@
 class ThinkingModesController < ApplicationController
-  def index
-  end
+  before_action :authenticate_user!
+
+  def index; end
 end
--- spec/requests/thinking_modes_spec.rb
+++ spec/requests/thinking_modes_spec.rb
@@ -2,8 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "ThinkingModes", type: :request do
   describe "GET /index" do
+    let(:user) { create(:user) }
+
+    before do
+      sign_in user
+    end
+
     it "returns http success" do
-      get "/thinking_modes/index"
+      get thinking_modes_path
+
       expect(response).to have_http_status(:success)
     end
   end
