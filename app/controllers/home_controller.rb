49a91f4b3e078525ae7d67c8fd6dc164d673afb2
takuya1 <oktkgm1997@icloud.com>
2026-07-27 19:17:02 +0900

    style: ホーム画面のUIをダッシュボード形式へ改善

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @remaining_ai_usage = AiUsageService.remaining_count(current_user)
+    @today_trainings_count = current_user.trainings.where(created_at: Time.zone.today.all_day).count
+  end
 end
