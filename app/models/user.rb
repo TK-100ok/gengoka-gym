fb777cb275e801785f1786d5675086fc29633b6e
takuya1 <oktkgm1997@icloud.com>
2026-08-21 09:43:48 +0900

    style: Lint修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable,
-         omniauth_providers: [:google_oauth2]
+         omniauth_providers: [ :google_oauth2 ]
 
   validates :name, presence: true
 
